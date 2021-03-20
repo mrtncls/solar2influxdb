c096bce39da5625ae5546b87804d692a159c82c0
mrtncls <mrtncls@gmail.com>
2021-03-20 14:34:15 +0100

    InfluxDB2 by default

--- src/Solar2InfluxDB.InfluxDB/InfluxDBConfig.cs
+++ src/Solar2InfluxDB.InfluxDB/InfluxDBConfig.cs
@@ -6,12 +6,20 @@
 
         public string Url { get; set; }
 
-        public string Username { get; set; }
+        public string Token { get; set; }
 
-        public string Password { get; set; }
+        public string Organization { get; set; }
 
-        public string Database { get; set; }
+        public string Bucket { get; set; }
 
-        public string RetentionPolicy { get; set; }
+        public bool V1 { get; set; }
+
+        public string V1_Username { get; set; }
+
+        public string V1_Password { get; set; }
+
+        public string V1_Database { get; set; }
+
+        public string V1_RetentionPolicy { get; set; }
     }
 }
--- src/Solar2InfluxDB.InfluxDB/InfluxExportClient.cs
+++ src/Solar2InfluxDB.InfluxDB/InfluxExportClient.cs
@@ -10,17 +10,26 @@ namespace Solar2InfluxDB.InfluxDB
     {
         private readonly ILogger<InfluxExportClient> logger;
         private readonly InfluxDBClient client;
+        private readonly InfluxDBConfig config;
 
         public InfluxExportClient(InfluxDBConfig config, ILogger<InfluxExportClient> logger)
         {
             this.logger = logger;
+            this.config = config;
 
-            client = InfluxDBClientFactory.CreateV1(
-                config.Url ?? throw new Exception("No InfluxDB url specified"),
-                config.Username ?? string.Empty,
-                config.Password?.ToCharArray() ?? Array.Empty<char>(),
-                config.Database ?? throw new Exception("No InfluxDB database specified"),
-                config.RetentionPolicy ?? "autogen");
+            if (config.V1)
+            {
+                client = InfluxDBClientFactory.CreateV1(
+                    config.Url ?? throw new Exception("No InfluxDB url specified"),
+                    config.V1_Username ?? string.Empty,
+                    config.V1_Password?.ToCharArray() ?? Array.Empty<char>(),
+                    config.V1_Database ?? throw new Exception("No InfluxDB database specified"),
+                    config.V1_RetentionPolicy ?? "autogen");
+            }
+            else
+            {
+                client = InfluxDBClientFactory.Create(config.Url, config.Token);
+            }
         }
 
         Task IMeasurementWriter.Initialize() => Task.CompletedTask;
@@ -29,7 +38,17 @@ namespace Solar2InfluxDB.InfluxDB
         {
             using (var writeApi = client.GetWriteApi())
             {
-                writeApi.WritePoint(measurements.ToPointData());
+                if (config.V1)
+                {
+                    writeApi.WritePoint(measurements.ToPointData());
+                }
+                else
+                {
+                    writeApi.WritePoint(
+                        config.Bucket,
+                        config.Organization,
+                        measurements.ToPointData());
+                }
 
                 logger.LogDebug($"Collection '{measurements.Name}' written to InfluxDB");
             }
